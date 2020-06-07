4986426d4808805f71bd8186e641ccc24ea49a92
Nora <nora.sayed@bluecrunch.com>
2020-06-07 12:40:39 +0200

    Initial Commit

---
+++ BotterSDK/BotterSDK.h
@@ -0,0 +1,19 @@
+//
+//  BotterSDK.h
+//  BotterSDK
+//
+//  Created by Nora on 6/7/20.
+//  Copyright © 2020 BlueCrunch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BotterSDK.
+FOUNDATION_EXPORT double BotterSDKVersionNumber;
+
+//! Project version string for BotterSDK.
+FOUNDATION_EXPORT const unsigned char BotterSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BotterSDK/PublicHeader.h>
+
+
